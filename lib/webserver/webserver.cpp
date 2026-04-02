1b3fdddbb7483f38ad7e433bd2fd41ec48d744b8
Quan Vu <66623851+vuquan2005@users.noreply.github.com>
2026-04-03 00:39:43 +0700

    fix: stackoverflow (add chunk respon)
    Signed-off-by: Quan Vu <66623851+vuquan2005@users.noreply.github.com>

--- lib/webserver/webserver.cpp
+++ lib/webserver/webserver.cpp
@@ -16,8 +16,31 @@ void WebManager::begin() {
     dnsServer.start(DNS_PORT, "*", WiFi.softAPIP());
 
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-        AsyncWebServerResponse *response = request->beginResponse(200, "text/html", html, html_len);
+        if (request->hasHeader("If-None-Match") &&
+            request->header("If-None-Match") == ETAG_STRING) {
+            request->send(304);
+            return;
+        }
+
+        AsyncWebServerResponse *response = request->beginChunkedResponse(
+            "text/html", [](uint8_t *buffer, size_t maxLen, size_t index) -> size_t {
+                if (index >= html_len) {
+                    return 0;
+                }
+
+                size_t chunkSize = maxLen;
+                if (index + chunkSize > html_len) {
+                    chunkSize = html_len - index;
+                }
+
+                memcpy_P(buffer, html + index, chunkSize);
+                return chunkSize;
+            });
+
         response->addHeader("Content-Encoding", "gzip");
+        response->addHeader("Cache-Control", "no-cache");
+        response->addHeader("ETag", ETAG_STRING);
+
         request->send(response);
     });
 
